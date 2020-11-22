6d407413f0103804ead1065fde2a0925ac69c439
AndreiShupayeu <shupaevandrei@tut.by>
2020-11-22 19:00:09 +0300

    изменение

--- src/Arrays2.java
+++ src/Arrays2.java
@@ -1,5 +1,9 @@
 public class Arrays2 {
     public static void main(String[] args) {
+        int[][] dArray = new int [3][];
+        for (int n =0; n<3; n++)
+            dArray[n] = new int [n +1];
+System.out.println(dArray);
 
     }
 }
